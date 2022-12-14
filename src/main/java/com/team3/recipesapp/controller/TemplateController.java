d842152859c45a22c545de4f6304d6b7e7bdf2e3
Salomeja Plukaite <s.plukaite@gmail.com>
2022-12-14 10:29:59 +0200

    add login and logout

---
+++ src/main/java/com/team3/recipesapp/controller/TemplateController.java
@@ -0,0 +1,19 @@
+package com.team3.recipesapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+    @GetMapping("login")
+    public String getLoginView() {
+        return "login";
+    }
+
+    @GetMapping("home")
+    public String getCourses() {
+        return "home";
+    }
+}
