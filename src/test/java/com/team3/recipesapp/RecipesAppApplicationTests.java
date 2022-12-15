fb1222e9980ee6d1e92c617ae6714d5a2cf9a0ff
Dainis Klavins <bonanzaaa.mad@gmail.com>
2022-12-15 15:21:59 +0200

    Turn the unit tests back on

--- src/test/java/com/team3/recipesapp/RecipesAppApplicationTests.java
+++ src/test/java/com/team3/recipesapp/RecipesAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.team3.recipesapp;
 
+import com.team3.recipesapp.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,52 +9,43 @@ import static org.mockito.Mockito.when;
 import java.sql.*;
 import org.mockito.Mock;
 import org.junit.jupiter.api.Assertions;
-//@SpringBootTest
+@SpringBootTest
 class RecipesAppApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//    @Test
-//    public void SimpleTest() {
-//        assertEquals(1,1);
-//    }
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void SimpleTest() {
+        assertEquals(1,1);
+    }
 
 
-//    @Mock
-//    private PreparedStatement insertStatement;
-//    @Mock
-//    private ResultSet resultSet;
+    @Mock
+    private PreparedStatement insertStatement;
+    @Mock
+    private ResultSet resultSet;
 
 
-//    @Mock
-//    private TestUser user;
+    @Mock
+    private User user;
 
-//    @Test
-//    public void check_a_user_properties(){
-////        TestUser testuser = new TestUser(1,"Alex", "12345");
-////
-////        assertThat(testuser).hasFieldOrPropertyWithValue("ID",1);
-////        assertThat(testuser).hasFieldOrPropertyWithValue("username","Alex");
-////        assertThat(testuser).hasFieldOrPropertyWithValue("password","12345");
-//    }
-//
-//    @Test
-//    public void Check() throws Exception{
-////        user = new TestUser();
-////
-////        when(resultSet.getInt(1)).thenReturn(user.getID());
-////        when(resultSet.getString(2)).thenReturn(user.getUsername());
-////        when(resultSet.getString(3)).thenReturn(user.getPassword());
-//    }
-//
-//    @Test
-//    public void setUserDetails() {
-////        user.setID(23);
-////        user.setPassword("1234564565");
-////        user.setUsername("Fredy");
-//    }
+    @Test
+    public void check_a_user_properties(){
+        User testuser = new User("testUser","Alex", "12345");
+
+        assertThat(testuser).hasFieldOrPropertyWithValue("email","Alex");
+        assertThat(testuser).hasFieldOrPropertyWithValue("username","testUser");
+        assertThat(testuser).hasFieldOrPropertyWithValue("password","12345");
+    }
+
+    @Test
+    public void setUserDetails() {
+        user.setPassword("1234564565");
+        user.setUsername("Fredy");
+        user.setEmail("fredy@gmail.com");
+    }
 
 
 
--- src/test/java/com/team3/recipesapp/UserRepositoryTest.java
+++ src/test/java/com/team3/recipesapp/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.team3.recipesapp;
 
+import com.team3.recipesapp.model.User;
+import com.team3.recipesapp.repository.UserRepository;
 import jakarta.annotation.Resource;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -11,62 +13,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 
-//@DataJpaTest
+@DataJpaTest
 public class UserRepositoryTest {
-//    @Autowired
-//    private UserRepository repository;
-//
-//    @Resource
-//    private EntityManager entityManager;
+    @Autowired
+    private UserRepository repository;
+
+    @Resource
+    private EntityManager entityManager;
 
 
 
 
-//    @Test
-//    public void no_users_in_repository(){
-//        //Iterable<User> users = repository.findAll();
-//        //assertThat(users).isEmpty();
-//
-//    }
+    @Test
+    public void no_users_in_repository(){
+        Iterable<User> users = repository.findAll();
+        assertThat(users).isEmpty();
+    }
 
-//    @Test
-//    public void save_and_find_user_in_repository(){
+    @Test
+    public void save_and_find_user_in_repository(){
 
-        //  User user = repository.save(new User( ,"Alex","alex@gmail.com","12345"));
-//        User user = new User();
-//        user.setUsername("Alex");
-//        user.setPassword("12345");
-//        user.setEmail("alex@gmail.com");
-//        repository.save(user);
-//
-//        assertThat(user).hasFieldOrPropertyWithValue("username","Alex");
-//        assertThat(user).hasFieldOrPropertyWithValue("email", "alex@gmail.com");
-//        assertThat(user).hasFieldOrPropertyWithValue("password","12345");
-//
+        User user = repository.save(new User("Alex","alex@gmail.com","12345"));
+        user.setUsername("Alex");
+        user.setPassword("12345");
+        user.setEmail("alex@gmail.com");
+        repository.save(user);
 
-    //}
+        assertThat(user).hasFieldOrPropertyWithValue("username","Alex");
+        assertThat(user).hasFieldOrPropertyWithValue("email", "alex@gmail.com");
+        assertThat(user).hasFieldOrPropertyWithValue("password","12345");
 
-//    @Test
-//    public void delete_all_users_in_repository(){
-
-//        User user = new User();
-//        user.setUsername("Alex");
-//        user.setPassword("12345");
-//        user.setEmail("alex@gmail.com");
-//        repository.save(user);
-//
-//        User user2 = new User();
-//        user.setUsername("Den");
-//        user.setPassword("abhfsg");
-//        user.setEmail("Den@gmail.com");
-//        repository.save(user2);
-//
-//        entityManager.persist(user);
-//        entityManager.persist(user2);
-//        repository.deleteAll();
-//        assertThat(repository.findAll()).isEmpty();
-
-    //}
-
-
+    }
+    
 }
