c874052485a2a2f710c16c9ae0cf1a8cb2046a62
Salomeja Plukaite <s.plukaite@gmail.com>
2022-12-15 14:07:28 +0200

    add redirection to login and signup pages refactor TemplateController to HomeController and RegistrationController

--- src/main/java/com/team3/recipesapp/controller/UserController.java
+++ src/main/java/com/team3/recipesapp/controller/UserController.java
@@ -1,20 +1,12 @@
 package com.team3.recipesapp.controller;
 
 import com.team3.recipesapp.model.User;
-import com.team3.recipesapp.repository.UserRepository;
 import com.team3.recipesapp.security.ApplicationSecurityConfig;
 import com.team3.recipesapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
